c3fa2bc413827809aad7512dafd083ea005d7240
Leonardo Jaques <jaques.projetos@outlook.com>
2024-05-27 17:10:25 -0300

    chore: Update ParametrosInvalidosException class
    This commit updates the ParametrosInvalidosException class to include constructors with detailed messages and causes. This will improve error handling when invalid parameters are passed to a method or constructor.

--- src/ParametrosInvalidosException.java
+++ src/ParametrosInvalidosException.java
@@ -1,11 +1,24 @@
-public class ParametrosInvalidosException extends Exception{
+/**
+ * This exception is thrown when invalid parameters are passed to a method or constructor.
+ */
+public class ParametrosInvalidosException extends Exception {
+
+    /**
+     * Constructs a new ParametrosInvalidosException with the specified detail message.
+     *
+     * @param message the detail message
+     */
     public ParametrosInvalidosException(String message) {
         super(message);
-
     }
 
+    /**
+     * Constructs a new ParametrosInvalidosException with the specified detail message and cause.
+     *
+     * @param message the detail message
+     * @param cause   the cause
+     */
     public ParametrosInvalidosException(String message, Throwable cause) {
         super(message, cause);
     }
-
 }
